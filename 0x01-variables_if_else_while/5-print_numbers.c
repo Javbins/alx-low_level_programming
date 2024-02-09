42f24be1286bfba28d9d1c18282a69b96fd050ce
javbins <calmus885@gmail.com>
2024-02-09 03:18:29 +0300

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ *
+ *
+ */
+int main(void)
+{
+	int o;
+
+	for (o = 0; o < 10; o++)
+		printf("%d", o);
+	printf("\n");
+	return (0);
+}
